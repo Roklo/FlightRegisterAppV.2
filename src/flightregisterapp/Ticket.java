78fa9764460366a7d82b33adf93b3290f510658d
Robin Thorholm <robinsth@stud.ntnu.no>
2017-03-07 18:32:56 +0100

    Håkons changes

--- src/flightregisterapp/Ticket.java
+++ src/flightregisterapp/Ticket.java
@@ -12,11 +12,11 @@ package flightregisterapp;
 public class Ticket
 {
 
-    private int ticketID;
-    private String seat;
-    private String passenger;
-    private String flight;
-    private String flightID;
+    private final int ticketID;
+    private final String seat;
+    private final String passenger;
+    private final String flight;
+    private final String flightID;
     private int price;
 
     public Ticket(String passenger, String flight, String seat, String flightID,
@@ -28,5 +28,40 @@ public class Ticket
         this.ticketID = 0;
         this.flightID = "";
         this.price = 0;
+    }
+
+    public int getTicketID()
+    {
+        return ticketID;
+    }
+
+    public String getSeat()
+    {
+        return seat;
+    }
+
+    public String getPassenger()
+    {
+        return passenger;
+    }
+
+    public String getFlight()
+    {
+        return flight;
+    }
+
+    public String getFlightID()
+    {
+        return flightID;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price = price;
     }
 }
