0829dd9ead40ca9c10e55bc3bfc716edd1c4ecb9
Robin Thorholm <robinsth@stud.ntnu.no>
2017-03-10 12:33:41 +0100

    TicketTest Junit added.

--- test/flightregisterapp/TicketTest.java
+++ test/flightregisterapp/TicketTest.java
@@ -14,10 +14,17 @@ import static org.junit.Assert.*;
  */
 public class TicketTest
 {
-    
-    public TicketTest()
-    {
-    }
+
+    Passenger instancePassenger = new Passenger("Chuck", "Finley",
+            "chuck.finley@gmail.com");
+
+    Flight instanceFlight = new Flight("#F242", "AES",
+            "OSL", 13, 30, 14, 35, 1, 2, 2016, 3, 4, 2017, 5, 6);
+
+    Seat instanceSeat = new Seat(5, "A");
+
+    Ticket instance = new Ticket(instancePassenger, instanceFlight,
+            instanceSeat, "#F242", 0001, 4500);
 
     /**
      * Test of getTicketID method, of class Ticket.
@@ -25,6 +32,10 @@ public class TicketTest
     @Test
     public void testGetTicketID()
     {
+        System.out.println("getTicketID");
+        int expResult = 0001;
+        int result = instance.getTicketID();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -33,6 +44,11 @@ public class TicketTest
     @Test
     public void testGetSeat()
     {
+        System.out.println("getSeat");
+
+        String expResult = "5A";
+        String result = instance.getSeat().getSeatId();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -41,6 +57,12 @@ public class TicketTest
     @Test
     public void testGetPassenger()
     {
+        System.out.println("getPassenger");
+
+        String expResult = "Chuck Finley";
+        String result = instance.getPassenger().getName();
+        assertEquals(expResult, result);
+
     }
 
     /**
@@ -49,6 +71,17 @@ public class TicketTest
     @Test
     public void testGetFlight()
     {
+        
+        System.out.println("getFlight");
+
+        String expResult = "#F242";
+        String expResultDestination = "AES";
+        String resultFlightID = instance.getFlight().getFlightID();
+        String resultDestinationAirport = instance.getFlight().
+                getDestinationAirport();
+        
+        assertEquals(expResult, resultFlightID);
+        assertEquals(expResultDestination, resultDestinationAirport);
     }
 
     /**
@@ -57,6 +90,10 @@ public class TicketTest
     @Test
     public void testGetFlightID()
     {
+        System.out.println("getFlightID");
+        String expResult = "#F242";
+        String result = instance.getFlightID();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -65,6 +102,10 @@ public class TicketTest
     @Test
     public void testGetPrice()
     {
+        System.out.println("getPrice");
+        int expResult = 4500;
+        int result = instance.getPrice();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -73,6 +114,13 @@ public class TicketTest
     @Test
     public void testSetPrice()
     {
+            System.out.println("setPrice");
+            instance.setPrice(5000);
+            
+        int expResult = 5000;
+        int result = instance.getPrice();
+        assertEquals(expResult, result);
+        
     }
-    
+
 }
