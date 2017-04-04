84f48ab1cf148088dbcfa021ecbfdb1bc25eb424
Håkon I L Haram <hakonilharam@hotmail.com>
2017-04-04 11:25:30 +0200

    Working.

--- temp/entity/Pilot.java
+++ temp/entity/Pilot.java
@@ -5,24 +5,27 @@ package entity;
  * @author Håkon Haram, Robin Thorholm, Bjørnar Tennfjord, Erlend Knudsen
  * @version 1.0 (14.02.2017)
  */
-public class CabinCrew extends Person
+public class Pilot extends Person
 {
 
+    private String certificateNumber;
     private String employeeID;
 
     /**
-     * Creates a new cabin crew given by his or hers forename, lastname, and an
-     * email address.
+     * Creates a new pilot given by his or hers forename, lastname, and an email
+     * address.
      *
      * @param foreName
      * @param lastName
      * @param emailAddress
+     * @param certificateNumber
      * @param employeeID
      */
-    public CabinCrew(String foreName, String lastName, String emailAddress,
-            String employeeID)
+    public Pilot(String foreName, String lastName, String emailAddress,
+            String certificateNumber, String employeeID)
     {
         super(foreName, lastName, emailAddress);
+        this.certificateNumber = certificateNumber;
         this.employeeID = employeeID;
     }
 
@@ -31,11 +34,11 @@ public class CabinCrew extends Person
     {
         return this.employeeID;
     }
-    
+
     @Override
     public String getCertificateNumber()
     {
-        return "The cabin crew does not have a certificate number.";
+        return this.certificateNumber;
     }
 
     @Override
